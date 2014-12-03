2f3140db72ad60801b3599c8798e6ac0e227f449
Keijiro Takahashi <keijiro@X1.local>
2014-12-03 10:45:45 +0900

    Initial commit.

---
+++ Assets/AsciiFx/AsciiFx.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class AsciiFx : MonoBehaviour
+{
+    public Color colorTint = Color.white;
+
+    [Range(0, 1)]
+    public float blendRatio = 1.0f;
+
+    [Range(0.5f, 10.0f)]
+    public float scaleFactor = 1.0f;
+
+    [SerializeField] Shader shader;
+
+    private Material _material;
+    
+    Material material {
+        get {
+            if (_material == null)
+            {
+                _material = new Material(shader);
+                _material.hideFlags = HideFlags.HideAndDontSave;   
+            }
+            return _material;
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.color = colorTint;
+        material.SetFloat("_Alpha", blendRatio);
+        material.SetFloat("_Scale", scaleFactor);
+        Graphics.Blit(source, destination, material);
+    }
+    
+    void OnDisable ()
+    {
+        if (_material) DestroyImmediate(_material);   
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+ASCII Fx
+--------
+
+This is an ASCII art shader originally made by [movAX13h][movAX13h].
+The original shader is available at [Shadertoy][Shadertoy].
+
+[Shadertoy]: https://www.shadertoy.com/view/lssGDj
+[movAX13h]:  https://www.shadertoy.com/user/movAX13h
